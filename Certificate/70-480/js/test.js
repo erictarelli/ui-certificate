24bed27c903ff6cef0f9e483e3696ac0a700ea59
Eric <eric.tarelli@gmail.com>
2020-05-24 12:07:45 -0300

    Se agrega actualizacion de metodos

---
+++ Certificate/70-480/js/test.js
@@ -0,0 +1,59 @@
+const service = {
+  name: ["Eric", "Patricia", "Florencia"],
+};
+const test = {
+  teacher: ["Eric", "Delia", "Indiana"],
+  result: [1, 2],
+};
+Vue.component("my-li", {
+  props: ["_name", "_id"],
+  template: `
+        <li :id="_id" v-on:click="deleteName(_id)">{{_name}}</li>
+    `,
+  methods: {
+    deleteName: function (index) {
+      console.log(index);
+      var liToDelete = document.getElementById(index);
+      liToDelete.parentElement.removeChild(liToDelete);
+    },
+  },
+});
+
+Vue.component("my-button", {
+  props: ["_show"],
+  template: `
+    <div>
+        <input type="button" value="End" v-on:click="getResult">
+    </div>
+    `,
+  methods: {
+    getResult: function () {
+      var listInput = document.getElementsByClassName("test");
+
+      for (let index = 0; index < listInput.length; index++) {
+        var element = listInput[index];
+        if (element.checked) vm.message = index;
+      }
+    },
+  },
+});
+
+Vue.component("my-input", {
+  props: ["_teacher", "_id"],
+  template: `
+    <div>
+        <input type="checkbox" :id="_id" :name="_id" class="test">
+        <label>{{_teacher}}</label>           
+    </div>        
+    `,
+});
+
+var vm = new Vue({
+  el: "main",
+  data: {
+    listNames: service.name,
+    listTeacher: test.teacher,
+    listResult: test.result,
+    message: 0,
+  },
+});
